afd73825dbacc65623e37f2d65bb4ea29bf291f8
Neruthes <i@neruthes.xyz>
2025-03-16 17:25:01 +0000

    Regular update '2025-03-16T17:25:01+00:00'

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ const _default_fancy_converters = {
         let out_str = pandoc_proc.stdout.toString();
         return out_str;
     },
-    markdown_table_to_pandoc: function (task_obj) {
+    markdown_to_pandoc: function (task_obj) {
         const pandoc_proc = child_process.spawnSync(`pandoc`, ['-f', 'markdown', '-t', 'latex'], {input: task_obj.cdata});
         let out_str = pandoc_proc.stdout.toString();
         return out_str;
